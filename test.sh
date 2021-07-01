37f5082ed856f2b074b991d4fc2aa58473f2540c
Greg Ward <greg@gerg.ca>
2021-07-01 13:30:37 -0400

    add unit tests

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+PYTHONPATH=lib; export PYTHONPATH
+pytest .
---
+++ tests/test_codespell.py
@@ -0,0 +1,25 @@
+import codespell
+
+
+class TestCodeChecker:
+    def test_split_line(self):
+        checker = codespell.CodeChecker()
+
+        tests = [
+            (
+                '"match = pat.search(current_line, 0, pos)"',
+                ['match', 'pat', 'search', 'current', 'line', 'pos']
+            ),
+            (
+                '_obj.doSomething(VALUE, FooBar + Blah_Foo)',
+                ['obj', 'do', 'Something', 'VALUE', 'Foo', 'Bar', 'Blah', 'Foo']
+            ),
+            (
+                'HTTPResponse getXMLElement',
+                ['HTTP', 'Response', 'get', 'XML', 'Element']
+            ),
+        ]
+
+        for (input, expect) in tests:
+            assert checker.split_line(input) == expect
+
